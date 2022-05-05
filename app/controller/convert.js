50a985f62e2a6310fd3c8437958366c6d258a228
graciela <graciela@gmail.com>
2022-05-05 19:17:55 -0400

    este esta biennnnnn node poppler2

--- app/controller/convert.js
+++ app/controller/convert.js
@@ -1,43 +1,48 @@
 const path = require('path');   
 const fs = require("fs");
-const pdf = require('pdf-poppler')
+const { Poppler } = require("node-poppler");
 const{guardarImagen, guardarImagenes}= require('../../config/db')
 
 
-//node-poppler2
+//empezando con node poppler2
+const poppler = new Poppler();
+
 const dir=`${__dirname}/../../pdfImageFolder`
 const absolutepath=path.join(path.resolve(dir))
 
-function convertImage(pdfPath) {
-    const NombreDB = path.basename(pdfPath, path.extname(pdfPath));
-    const carpetaGuardado= fs.mkdirSync(path.join(absolutepath, NombreDB));
-    let option = {
-        format : 'jpeg',
-        out_dir: path.join(absolutepath, NombreDB),
-        out_prefix : path.basename(pdfPath, path.extname(pdfPath)),//guarde en carpetas
-        //almacenar absolutepath y outprefix en la base de datos
-        //page : 1
-    }
-// option.out_dir valor donde el archivo estara guardado
-//pdfpath es la ruta del archivo que se va cambiar
-    //console.log(option.out_prefix)
-    pdf.convert(pdfPath, option)
-    .then(async(e) => {
-        console.log('file converted??????', e)
-          //await guardarDocumento_img(NombreDB,option.out_dir);
-        //aqui porque esta llamando mas antes-------
+function convertImage(pdfPath){  
+    const file = pdfPath;
+    const NombreDB = path.basename(pdfPath, path.extname(pdfPath));//nombre de la carpeta que se esta guardando
+     fs.mkdirSync(path.join(absolutepath, NombreDB));
+    console.log("ESTE ES EL NOMBRE DE BASE DE DATOS"+ NombreDB);
+    const rutaGuradado=path.join(absolutepath,NombreDB);//ruta donde va estar guardado
+    const options = {    
+        pngFile: true,
+    };
 
+    const outputFile =path.join(rutaGuradado,path.basename(pdfPath));//El nombre con el que va salir
+    console.log("---AQUI LOS ARCHIVOS "+outputFile);
 
-        fs.readdir(option.out_dir, (e, f)=>{
+    poppler.pdfToCairo(file, outputFile, options)
+    
+    .then(async(res)=>{
+        console.log(res);
+        fs.readdir(path.join(absolutepath, NombreDB), (e, f)=>{
             console.log('ARCHIVOSSSSS',f)
             f.map(async(x)=>{
-                await guardarImagen( x, option.out_dir)
+                await guardarImagen( x, path.join(absolutepath, NombreDB))
             })
         })
-    })
-    .catch(err => {
-        console.log('an error has occurred in the pdf converter ' + err)
-    })
+    });
+
 }
 
+
 module.exports.convertImage=convertImage;
+
+
+
+
+
+
+
